81afbe1f57f5ce3473ca0044c775c18b53b24ad4
Constantin Buruiana <constbur@gmail.com>
2019-01-01 20:06:59 -0500

    Fixed (?) registration script.

--- Development/Backend/PHP/registration.php
+++ Development/Backend/PHP/registration.php
@@ -1,12 +1,16 @@
 <?php
     $id = uniqid("", true);
-    $user = $_POST['user'];
-    $psw = hash('sha256', $_POST['psw']);
-
+    $user = "";
+    $psw = "";
     function sendData() {
         global $id, $user, $psw;
-        if(mysql_query("INSERT INTO Users 
-        (AccountId, UserName, PasswordHash) VALUES('$id', '$user', '$psw')"))
+        $id = uniqid("", true);
+        if(isset($_POST['user']))
+            $user = $_POST['user'];
+        if(isset($_POST['psw']))
+            $psw = hash('sha256', $_POST['psw']);
+        if($user != "" && $psw != "" && 
+        mysqli_query($conn, "INSERT INTO Users (AccountId, UserName, PasswordHash) VALUES('$id', '$user', '$psw')"))
             echo "Registration successful!";
         else
             echo "Registration failed"; }
