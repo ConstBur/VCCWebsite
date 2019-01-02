a51c0a8368ebf6855efb44a4af94b9984ae8bb93
Constantin Buruiana <constbur@gmail.com>
2019-01-02 18:22:57 -0500

    Fixed (?) PHP script.

--- Development/Backend/PHP/registration.php
+++ Development/Backend/PHP/registration.php
@@ -1,5 +1,5 @@
 <?php
-    $id = uniqid("", true);
+    $id = "";
     $user = "";
     $psw = "";
     include_once('phpconnect.php');
@@ -7,9 +7,15 @@
         global $id, $user, $psw;
         $id = uniqid("", true);
         if(isset($_POST['username']))
+        {
             $user = $_POST['username'];
+            echo "User $user obtained.\n";
+        }
         if(isset($_POST['psw']))
+        {
             $psw = hash('sha256', $_POST['psw']);
+            echo "Password hash $psw obtained.\n";
+        }
         if($user != "" && $psw != "" && 
         mysqli_query($conn, "INSERT INTO Users (AccountId, UserName, PasswordHash) VALUES('$id', '$user', '$psw')"))
             echo "Registration successful!";
