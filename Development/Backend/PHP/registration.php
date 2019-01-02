a48657a0136004449bbd121f0a73887e7159aedc
Constantin Buruiana <constbur@gmail.com>
2019-01-01 19:52:18 -0500

    Fixed (?) PHP scripts.

--- Development/Backend/PHP/registration.php
+++ Development/Backend/PHP/registration.php
@@ -1,13 +1,15 @@
 <?php
-    include_once('phpconnect.php');
-
     $id = uniqid("", true);
     $user = $_POST['user'];
     $psw = hash('sha256', $_POST['psw']);
 
-    if(mysql_query("INSERT INTO Users 
-    (AccountId, UserName, PasswordHash) VALUES('$id', '$user', '$psw')"))
-        echo "Registration successful!";
-    else
-        echo "Registration failed";
+    function sendData() {
+        global $id, $user, $psw;
+        if(mysql_query("INSERT INTO Users 
+        (AccountId, UserName, PasswordHash) VALUES('$id', '$user', '$psw')"))
+            echo "Registration successful!";
+        else
+            echo "Registration failed"; }
+    include_once('phpconnect.php');
+    sendData();
 ?>
