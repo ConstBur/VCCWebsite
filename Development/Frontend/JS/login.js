629ddaaadccc1a718c92694e363166fe3d3af36e
Constantin Buruiana <constbur@gmail.com>
2019-01-03 18:23:22 -0500

    Fixed (?) PHP script.

--- Development/Frontend/JS/login.js
+++ Development/Frontend/JS/login.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $("button").click(function() {
         $("#result").html("Logging in...");
-		var data = $("#regForm").serializeArray();
+		var data = $("#loginForm").serializeArray();
 		console.log(data);
 		$.post($("#loginForm").attr("action"), data, function(info) {
             $("#result").html(info);
