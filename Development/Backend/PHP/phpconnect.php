f9b28d28c7103e6af44e27c9ddddf79b8aac8cdd
Constantin Buruiana <constbur@gmail.com>
2019-01-02 21:21:23 -0500

    Fixed (?) PHP script.

--- Development/Backend/PHP/phpconnect.php
+++ Development/Backend/PHP/phpconnect.php
@@ -8,8 +8,8 @@
     $conn = mysqli_connect($servername, $username, $password, $database);
 
     // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
     }
     echo "Connected successfully";
 ?>
