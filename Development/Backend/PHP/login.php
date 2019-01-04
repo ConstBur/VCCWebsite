f967dbe860f8aedfc28bca3a7e9919bb7a7036a2
Constantin Buruiana <constbur@gmail.com>
2019-01-04 13:59:02 -0500

    Fixed (?) PHP script.

--- Development/Backend/PHP/login.php
+++ Development/Backend/PHP/login.php
@@ -2,21 +2,21 @@
     $user = "";
     $psw = "";
     include_once('phpconnect.php');
-    function checkUser() {
+    function checkUser() 
+    {
         global $user, $psw, $conn;
         if(isset($_POST['username']))
             $user = $_POST['username'];
         if(isset($_POST['psw']))
             $psw = hash('sha256', $_POST['psw']);
-        if(mysqli_query($conn, "SELECT AccountId FROM Accounts WHERE UserName = '$user';"))
-        {
-            if(mysqli_query($conn, "SELECT AccountId FROM Accounts WHERE PasswordHash = '$psw'"))
-                echo "Login Successful!";
-            else
-                echo "Wrong password!";
-        }
-        else
+        $query = mysqli_query($conn, "SELECT PasswordHash FROM Accounts WHERE UserName = '$user';");
+        $count = mysqli_num_rows($query);
+        if(count == "0")
             echo "User not found!";
+        else if(mysqli_fetch_array($query)[0] != $psw)
+            echo "Wrong password!";
+        else
+            echo "Login successful!";
     }
     checkUser();
 ?>
